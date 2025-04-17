d5a7ee72745f9dd987700c323d0699e88da60a99
NotTooShabby <spbbartels@gmail.com>
2025-04-16 23:56:51 -0400

    Added a tutorial command with delayed messsaging!

--- src/main/java/me/scott/codkillstreaksplugin/CodKillstreakCommandHandler.java
+++ src/main/java/me/scott/codkillstreaksplugin/CodKillstreakCommandHandler.java
@@ -1,8 +1,11 @@
 package me.scott.codkillstreaksplugin;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class CodKillstreakCommandHandler implements CommandExecutor {
     private CodKillstreaksPlugin mainPlugin;
@@ -13,6 +16,38 @@ public class CodKillstreakCommandHandler implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        if (!(commandSender instanceof Player player)) {
+            mainPlugin.getLogger().info("Sender of command must be a player!");
+            return true;
+        }
+
+        // tutorial command
+
+        new BukkitRunnable() {
+            int ticks = 0;
+
+            @Override
+            public void run() {
+                switch(ticks) {
+                    case 0:
+                        player.sendMessage(ChatColor.YELLOW + "This is a call of duty killstreaks plugin for Minecraft");
+                        break;
+                    case 60:
+                        player.sendMessage(ChatColor.YELLOW + "Try to get the best killstreak possible to earn those unique call of duty killstreaks!");
+                        break;
+                    case 110:
+                        player.sendMessage(ChatColor.GOLD + "Good luck!");
+                        break;
+                    case 120:
+                        this.cancel();
+                        break;
+                }
+
+                ticks++;
+            }
+        }.runTaskTimer(mainPlugin, 0, 1L);
+
+
 
         return true;
     }
